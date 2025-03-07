fceb9c887c56f574e0e0c275705b5129c93591f3
Bkyb <130330621+Bkyb@users.noreply.github.com>
2025-03-07 19:53:51 +0900

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -316,7 +316,6 @@ class DetectionModel(BaseModel):
         # Define model
         ch = 4# self.yaml["ch"] = self.yaml.get("ch", ch)  # input channels
         self.yaml["ch"] = 4
-        LOGGER.info(f"the input channels depth is ... {ch}")
         if nc and nc != self.yaml["nc"]:
             LOGGER.info(f"Overriding model.yaml nc={self.yaml['nc']} with nc={nc}")
             self.yaml["nc"] = nc  # override YAML value
