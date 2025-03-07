6c2f8419c579a8f3e001cf516ee1f323d413b914
Bkyb <130330621+Bkyb@users.noreply.github.com>
2025-03-07 18:27:42 +0900

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -64,6 +64,7 @@ from ultralytics.nn.modules import (
     WorldDetect,
     v10Detect,
     SilenceChannel,
+    Gif,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -86,7 +87,7 @@ from ultralytics.utils.torch_utils import (
     scale_img,
     time_sync,
 )
-from ultralytics.nn.modules.gif import Gif
+
 
 try:
     import thop
